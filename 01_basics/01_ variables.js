aa1bd74b5fc3453671a3a83a17918fccd7913d74
avijit <freelanc9078@gmail.com>
2025-07-14 18:25:36 +0000

    To day I have complited in const,let and var

---
+++ 01_basics/01_ variables.js
@@ -0,0 +1,16 @@
+const accountId = 113345;
+let accountEmail='avi@gmail.com';
+var accountPassword='12345';
+accountCity='bhubaneswar';
+ 
+
+// accountId= 2 Not allowed, as const cannot be reassigned //
+console.log(accountId);
+
+accountEmail='jiy@gmail.com';
+accountPassword='jit123!';
+accountCity='delhi';
+let accountState;
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
