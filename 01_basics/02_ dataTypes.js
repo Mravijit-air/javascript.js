633f26af6d55661543de176e1428126e25fc9361
avijit <freelanc9078@gmail.com>
2025-07-15 18:04:39 +0000

    data type conversion

---
+++ 01_basics/02_ dataTypes.js
@@ -0,0 +1,21 @@
+'use strict' //Treaa all js code as newse version
+// alert('Hii I ma Avijit') We are Using nodejs, Not browser
+
+// Type of datTypes
+let name = 'avijit'
+let age = 22
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// Object
+// console.log(typeof age);
+// console.log(typeof null); object
+// console.log(typeof undefined); undefined
+
