6bc716ed548145e712064355c7bbb0489f9c1b19
avijit <freelanc9078@gmail.com>
2025-07-20 11:03:30 +0000

    all file code change

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,10 @@
+// ++++++++++++++++++++++++++++++++++++++++++++++
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubeName = "hiteshchoudharydotcom"
+
+let anothername = myYoutubeName
+anothername = "chaiaurcode"
+
+console.log(myYoutubeName);
+console.log(anothername);
